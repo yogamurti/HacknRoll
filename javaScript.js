55c72b0aa09634f130bbb3781cf020a79cba6ea2
noobistz <noobistz@users.noreply.github.com>
2017-01-22 02:38:17 +0800

    Add files via upload

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,197 @@
 // ArrestedDevelopment
 
+function info0() {
+  $('#graph').highcharts({
+      chart: {
+        backgroundColor: "#17202A",
+        spacingTop: 24,
+        style: {
+          fontSize: 12,
+          fontFamily: 'Oxygen, sans-serif',
+          fontWeight: 300,
+        }
+      },
+      navigation: {
+        buttonOptions: {
+          theme: {
+            fill: '#333333',
+            stroke: '#000',
+            states: {
+              hover: {
+                  fill: '#474747',
+                  stroke: '#333',
+              },
+              select: {
+                  fill: '#474747',
+                  stroke: '#333',
+              }
+            }
+          }
+        }
+     },
+     plotOptions: {
+       series: {
+         connectNulls: true
+       }
+     },
+      yAxis: {
+        allowDecimals: false,
+        title: {
+          text: "Lowest Successful Bid Point",
+          style: {
+            color: "#FFF",
+            fontWeight: 300,
+          }
+        },
+        labels: {
+          style: {
+            color: "#999",
+          }
+        },
+        gridLineColor: 'rgba(255, 255, 255, .1)'
+      },
+      xAxis: {
+        categories: ['AY14/15S1','AY14/15S2','AY15/16S1','AY15/16S2','AY16/17S1','AY16/17S2'],
+        tickColor: "#333", // hide ticks
+        lineColor: "#333", // hide line
+        title: {
+          text: "Semester",
+          style: {
+            color: "#FFF",
+            fontWeight: 300,
+          }
+        },
+        labels: {
+          style: {
+            color: "#999",
+          }
+        }
+      },
+      title: {
+        text: $('<div/>').html('ACC1002X').text(),
+        style: {
+          color: "#FFF",
+          font: '26px Lucida Sans Unicode',
+          fontWeight: 300,
+        }
+      },
+      legend: {
+           layout: 'vertical',
+           align: 'right',
+           verticalAlign: 'middle',
+           borderWidth: 0,
+           itemStyle: {
+            color: "#FFF",
+        }
+       },
+        tooltip: {
+            valueSuffix: ' Points'
+        },
+      series: [
+      {
+        name: 'Lecture 1',
+        data: [
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       0,
+          }
+        ],
+        marker: {
+            radius: 4,
+            symbol: "circle"
+        }
+      },
+      {
+        name: 'Lecture 2',
+        data: [
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       1,
+          }
+        ],
+        marker: {
+            radius: 4,
+            symbol: "circle"
+        }
+      },
+      {
+        name: 'Lecture 3',
+        data: [
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       0,
+          }
+        ],
+        marker: {
+            radius: 4,
+            symbol: "circle"
+        }
+      }
+      ]
+  });
+};
+
+
 function info1() {
   $('#graph').highcharts({
       chart: {
@@ -779,6 +971,131 @@ function info5() {
 
           {
             y:       1,
+          }
+        ],
+        marker: {
+            radius: 4,
+            symbol: "circle"
+        }
+      }
+      ]
+  });
+};
+
+function info6() {
+  $('#graph').highcharts({
+      chart: {
+        backgroundColor: "#17202A",
+        spacingTop: 24,
+        style: {
+          fontSize: 12,
+          fontFamily: 'Oxygen, sans-serif',
+          fontWeight: 300,
+        }
+      },
+      navigation: {
+        buttonOptions: {
+          theme: {
+            fill: '#333333',
+            stroke: '#000',
+            states: {
+              hover: {
+                  fill: '#474747',
+                  stroke: '#333',
+              },
+              select: {
+                  fill: '#474747',
+                  stroke: '#333',
+              }
+            }
+          }
+        }
+     },
+     plotOptions: {
+       series: {
+         connectNulls: true
+       }
+     },
+      yAxis: {
+        allowDecimals: false,
+        title: {
+          text: "Lowest Successful Bid Point",
+          style: {
+            color: "#FFF",
+            fontWeight: 300,
+          }
+        },
+        labels: {
+          style: {
+            color: "#999",
+          }
+        },
+        gridLineColor: 'rgba(255, 255, 255, .1)'
+      },
+      xAxis: {
+        categories: ['AY14/15S1','AY14/15S2','AY15/16S1','AY15/16S2','AY16/17S1','AY16/17S2'],
+        tickColor: "#333", // hide ticks
+        lineColor: "#333", // hide line
+        title: {
+          text: "Semester",
+          style: {
+            color: "#FFF",
+            fontWeight: 300,
+          }
+        },
+        labels: {
+          style: {
+            color: "#999",
+          }
+        }
+      },
+      title: {
+        text: $('<div/>').html('CS1010S').text(),
+        style: {
+          color: "#FFF",
+          font: '26px Lucida Sans Unicode',
+          fontWeight: 300,
+        }
+      },
+      legend: {
+           layout: 'vertical',
+           align: 'right',
+           verticalAlign: 'middle',
+           borderWidth: 0,
+           itemStyle: {
+            color: "#FFF",
+        }
+       },
+        tooltip: {
+            valueSuffix: ' Points'
+        },
+      series: [
+      {
+        name: 'Lecture 1',
+        data: [
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       700,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
+          },
+
+          {
+            y:       null,
           }
         ],
         marker: {
