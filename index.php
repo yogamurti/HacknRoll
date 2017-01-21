9d75fccd669a9b1ce29f2acfa06ec1497f509038
yogamurti <yogas789@hotmail.com>
2017-01-22 02:48:37 +0800

    Add files via upload
    proper formatting

--- index.php
+++ index.php
@@ -5,50 +5,77 @@
 
 <html>
 <head>
-  <title>CORS Bidding History</title>
-  <meta name="viewport" content="width=device-width">
+  <title>Hack n Roll 2017</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
   <link rel="stylesheet" href="stylesheet.css">
-  <nav class='navbar navbar-inverse navbar-fixed-top'>
-    <div class='container'>
-      <div class='navbar-header'>
-        <button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
-          <span class='sr-only'>Toggle navigation</span>
-          <span class='icon-bar'></span>
-          <span class='icon-bar'></span>
-          <span class='icon-bar'></span>
-        </button>
-      </div>
-      <div id='navbar' class='collapse navbar-collapse'>
-        <ul class='nav navbar-nav'></ul>
-        <?php
-          echo "<form class='navbar-form' method='post'>;
-          <div class='form-group'>
-            <div id='MC' style='float:left'><input placeholder='Module Code' class='form-control' name='ModuleCode' type='text'></div>
-            <div id='FAC' style='float:left'><input placeholder='Faculty' class='form-control' name='Faculty' type='text'></div>
-            <div id='BR' style='float:left'><input placeholder='Bid Round' class='form-control' name='BiddingRound' type='text'></div>
-            <div id='SAT' style='float:left'><input placeholder='Student & Account Type' class='form-control' name='StudentAccountType' type='text'></div>
-          </div>
-          <button onclick='go()' class='btn btn-default'>Go!</button>
-        </form>";
-        ?>
-      </div>
-    </div>
-  </nav>
 </head>
 <body>
+  <br/>
   <div class="mainHeading">CORS Bidding History</div>
-    <br/>
+    <div class='container'>
+      <div class='topbar'>
+        <div style = "font-family: Times New Roman; font-size: 20px";>
+          <div class='tocenter'>
+            <div id='wrap'>
+              <center>
+              <ul class='menu'>
+                <li>
+                  <a href="#"><b>Bidding Round ↓</a></b>
+                  <ul>
+                    <li><a href="#"><b>1A</b></a></li>
+                    <li><a href="#"><b>1B</b></a></li>
+                    <li><a href="#"><b>2A</b></a></li>
+                    <li><a href="#"><b>2B</b></a></li>
+                    <li><a href="#"><b>3A</b></a></li>
+                    <li><a href="#"><b>3B</b></a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#"><b>Module Code ↓</a></b>
+                  <ul>
+                    <li><a href="#"><b>ACC1002X</b></a></li>
+                    <li><a href="#"><b>CS1010S</b></a></li>
+                    <li><a href="#"><b>EC1301</b></a></li>
+                    <li><a href="#"><b>MKT1003X</b></a></li>
+                    <li><a href="#"><b>PL1101E</b></a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#"><b>Faculty ↓</a></b>
+                  <ul>
+                    <li><a href="#"><b>School of Computing</b></a></li>
+                    <li><a href="#"><b>School of Science</b></a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#"><b>Account Type ↓</a></b>
+                  <ul>
+                    <li><a href="#"><b>Programme</b></a></li>
+                  </ul>
+                </li>
+              </ul>
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      <!--<form method='post'>
+        <div id='ModuleCode' style='float:left'><input type="text" placeholder='Module Code' class='form-control'></div>
+        <div id='Faculty' style='float:left'><input placeholder='Faculty' class='form-control' type='text'></div>
+        <div id='BiddingRound' style='float:left'><input placeholder='Bid Round' class='form-control' type='text'></div>
+        <div id='StudentAccountType' style='float:left'><input placeholder='Student & Account Type' class='form-control' type='text'></div>
+        <button onclick='go()' class='btn btn-default'>Go</button>-\
+      </form>
   <div class="shows">
     <button onclick='info()' class='optionButton'>ACC1002X</button>
-  </div>
-    <br/>
+  </div>-->
+  <br/>
   <div id="graph"></div>
   <script src="javaScript.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="https://code.highcharts.com/highcharts.js"></script>
   <script src="https://code.highcharts.com/modules/exporting.js"></script>
-  <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
